93f79abb066a093e04e8419ce9d672e5d6055380
ManuelGuio09 <maguioc@correo.udistrital.edu.co>
2022-10-08 11:59:10 -0500

    .

--- pd_sim.py
+++ pd_sim.py
@@ -1,23 +1,28 @@
 class DataFrame:
     def __init__(self,data,*args,**kwargs):
         self.data=data
+        
+    def __getitem__(self,i):
+        print ("indice",i) 
 
 
 
-def read_csv(arch,sep=','):
-    arch = open(arch)
-    linea=arch.readline()
+def read_csv(filepath_or_buffer,sep=','):
+    filepath_or_buffer = open(filepath_or_buffer)
+    linea=filepath_or_buffer.readline()
     data={}
     titulos=[]
     for titulo in linea.split(sep):
+        titulo=titulo.strip()
         data[titulo]=[]
         titulos.append(titulo)
-    for linea in arch.readlines():
+    for linea in filepath_or_buffer.readlines():
         for i,elem in enumerate(linea.split(sep)):
-            data[titulos[i]].append(elem)
+            data[titulos[i]].append(elem.strip())
     return DataFrame(data)
             
         
 #prueba
-#d=read_csv('datos.csv')
-#print(d.data)
+d=read_csv('datos.csv')
+print(d.data)
+
