d4bf79bc46dcf03bdda0304241aa7c3eed11ce81
Gerardo Munoz <36160037+GerardoMunoz@users.noreply.github.com>
2022-10-08 07:45:50 -0500

    Add files via upload

---
+++ pd_sim.py
@@ -0,0 +1,23 @@
+class DataFrame:
+    def __init__(self,data,*args,**kwargs):
+        self.data=data
+
+
+
+def read_csv(arch,sep=','):
+    arch = open(arch)
+    linea=arch.readline()
+    data={}
+    titulos=[]
+    for titulo in linea.split(sep):
+        data[titulo]=[]
+        titulos.append(titulo)
+    for linea in arch.readlines():
+        for i,elem in enumerate(linea.split(sep)):
+            data[titulos[i]].append(elem)
+    return DataFrame(data)
+            
+        
+#prueba
+#d=read_csv('datos.csv')
+#print(d.data)
