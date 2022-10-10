92d44e3c61fb05cc0d937d7877017110bf28f40a
Fabian Camargo <flcamargob@correo.udistrital.edu.co>
2022-10-09 23:22:05 -0500

    funcion tail

--- pd_sim.py
+++ pd_sim.py
@@ -2,22 +2,35 @@ class DataFrame:
     def __init__(self,data,*args,**kwargs):
         self.data=data
 
-
-
-def read_csv(arch,sep=','):
+def funcion_tail(arch, n,sep=';'):
+	with open(arch) as f:
+		lineas = [lineas.strip('\n') for lineas in f.readlines()]
+	return lineas[-n:]
+ 
+ 
+def read_csv(arch,sep=';'):
     arch = open(arch)
     linea=arch.readline()
     data={}
     titulos=[]
     for titulo in linea.split(sep):
+        titulo=titulo.strip()
         data[titulo]=[]
         titulos.append(titulo)
+        
     for linea in arch.readlines():
         for i,elem in enumerate(linea.split(sep)):
-            data[titulos[i]].append(elem)
-    return DataFrame(data)
-            
-        
+            data[titulos[i]].append(elem.strip())
+    return DataFrame(data)           
 #prueba
-#d=read_csv('datos.csv')
-#print(d.data)
+d=read_csv('datos.csv')
+#x=sum(map(len,d.data.values()))
+#print(x)
+
+
+for t in d.data:
+   print (t, ":", d.data[t])
+   
+
+  
+print ("\n",funcion_tail("datos.csv",5))  
