cfd24c39d7a1e42790903a4e20fda2580c60b865
Manuel3gam <magamboar@correo.udistrital.edu.co>
2022-10-10 13:40:18 -0500

    comentarios acerca del funconamiento de la funcion

--- pd_sim.py
+++ pd_sim.py
@@ -6,10 +6,6 @@ class DataFrame:
         print('indice',indice)
         if isinstance(indice,slice):
             print('slice',indice.start,indice.step,indice.stop)
-    def loc(self,columna,fila=None):
-        dat=Dataframe
-        fila=Dataframe
-        return dat
 def read_csv(arch,sep=','):
     arch = open(arch)
     linea=arch.readline()
@@ -23,13 +19,15 @@ def read_csv(arch,sep=','):
         for i,elem in enumerate(linea.split(sep)):
             data[titulos[i]].append(elem.strip())
     return DataFrame(data)
-            
+def loc(self,columna,fila=None): #Funcion encargada de localizar filas y columnas del documento e identificar si la entrada es un int,str o slice. Imprimiento los datos.
+        dat=titulo
+        fila=titulo            
         
 #prueba
 d=read_csv('datos.csv')
 print(d.data)
 print(d[6])
 print(d[3:9:2])
-print('columna' + dat)
+print('columna')
 print(d[:])
 
