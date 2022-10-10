a03b7c1bb9a9b48881c7348093084439e19d2ae3
Manuel3gam <magamboar@correo.udistrital.edu.co>
2022-10-10 13:38:30 -0500

    comentarios acerca del funcionamiento de la funcion

--- pd_sim.py
+++ pd_sim.py
@@ -1,23 +1,35 @@
 class DataFrame:
     def __init__(self,data,*args,**kwargs):
         self.data=data
-
-
-
+        
+    def __getitem__(self, indice):
+        print('indice',indice)
+        if isinstance(indice,slice):
+            print('slice',indice.start,indice.step,indice.stop)
+    def loc(self,columna,fila=None):
+        dat=Dataframe
+        fila=Dataframe
+        return dat
 def read_csv(arch,sep=','):
     arch = open(arch)
     linea=arch.readline()
     data={}
     titulos=[]
     for titulo in linea.split(sep):
+        titulo=titulo.strip()
         data[titulo]=[]
         titulos.append(titulo)
     for linea in arch.readlines():
         for i,elem in enumerate(linea.split(sep)):
-            data[titulos[i]].append(elem)
+            data[titulos[i]].append(elem.strip())
     return DataFrame(data)
             
         
 #prueba
-#d=read_csv('datos.csv')
-#print(d.data)
+d=read_csv('datos.csv')
+print(d.data)
+print(d[6])
+print(d[3:9:2])
+print('columna' + dat)
+print(d[:])
+
