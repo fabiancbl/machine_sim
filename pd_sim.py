5111a9d5595b7b93edee758ee06b0d6a03b96952
Mateoo0003 <mateoosp.003@gmail.com>
2022-10-10 13:34:22 -0500

    Se creó la función T en la clase DataFrame para comenzar a trabajar allí

--- pd_sim.py
+++ pd_sim.py
@@ -1,8 +1,12 @@
+import numpy as np
+
 class DataFrame:
     def __init__(self,data,*args,**kwargs):
         self.data=data
-
-
+        
+    def T(self):
+        
+        print("Hola Mundo")
 
 def read_csv(arch,sep=','):
     arch = open(arch)
@@ -16,8 +20,23 @@ def read_csv(arch,sep=','):
         for i,elem in enumerate(linea.split(sep)):
             data[titulos[i]].append(elem)
     return DataFrame(data)
-            
-        
+
+# d1 = {
+#   "Nombre": "Sara",
+#   "Edad": 27,
+#   "Documento": 1003882
+# }
+
+# df=pd.DataFrame()
+# nombres = ['Juan', 'Laura', 'Pepe']
+# edades = [42, 40, 37]
+# df['Nombre'] = nombres
+# df['Edad'] = edades
+# print(df)
+
 #prueba
-#d=read_csv('datos.csv')
-#print(d.data)
+
+d=read_csv('datos.csv')
+print(d.data)
+
+d.T()
