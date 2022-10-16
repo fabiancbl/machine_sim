246da01485da88c72efc0a708ae9eec341cb3dd5
Oscar <odpobladorp@correo.udistrital.edu.co>
2022-10-16 00:39:09 -0500

    Primera función Iloc

--- pd_sim.py
+++ pd_sim.py
@@ -10,35 +10,15 @@ class DataFrame:
 
     def iloc(self,i):
 
-
         if isinstance (i,int):
             print("entero")
             titulos=self.data.keys()
+            for j in titulos:
+                valores=self.data[j]
+                print ("Titulo: ", j)
+                print("Dato solicitado: ",valores[i])
+                print("\n")
 
-            for i in titulos:
-                valores=self.data[i]
-                print(titulos)
-                print (valores)
-            
-            for i in range(0,i):
-             print(self.data[i])
-
-            datos=self.data
-            #a=datos(titulos[0])
-            #print(a)
-           # print(datos)
-            #a=datos[0]
-            #for j in range (len(titulos)):
-             #   imprimir[j]=list(datos[1])
-            #print(titulos)
-           # print(a[0])
-           # for j in range(len(titulos)):
-                #print(titulos[j])
-                #print(datos[i])
-              
-                 
-
-             #   a=d.data[i][0];
 
 def read_csv(arch,sep=','):
     arch = open(arch)
@@ -60,11 +40,8 @@ def read_csv(arch,sep=','):
 datos="hola.csv"       
 d=read_csv(datos)
 #d=DataFrame([1,'a','b','c',8,9,10])
-
-#print(d.data)
-d.iloc(5)
-
 print(d.data)
+d.iloc(1)
 
 #d.iloc(5,0)
 
@@ -73,10 +50,10 @@ print(d.data)
 
 
 
-#                   A         B         C         D
-##2013-01-01  0.469112 -0.282863 -1.509059 -1.135632
-#2013-01-02  1.212112 -0.173215  0.119209 -1.044236
-#2013-01-03 -0.861849 -2.104569 -0.494929  1.071804
-#*2013-01-04  0.721555 -0.706771 -1.039575  0.271860
-#2013-01-05 -0.424972  0.567020  0.276232 -1.087401
-#2013-01-06 -0.673690  0.113648 -1.478427  0.524988
+#    A         B         C         D
+#  0.469112, -0.282863, -1.509059, -1.135632
+# 1.212112, -0.173215,  0.119209, -1.044236
+# -0.861849, -2.104569, -0.494929,  1.071804
+#  0.721555, -0.706771, -1.039575,  0.271860
+# -0.424972,  0.567020,  0.276232, -1.087401
+# -0.673690,  0.113648, -1.478427,  0.524988
